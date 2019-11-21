7b646c38db7c5ace0800dd828c8e18ed537f346b
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-11-21 08:59:21 -0500

    start super simple console doc class to try out AST nodes

--- Sources/SwiftCodeHelper/ParserMain/ConsoleDocumentingVisitor.swift
+++ Sources/SwiftCodeHelper/ParserMain/ConsoleDocumentingVisitor.swift
@@ -5,8 +5,19 @@ public class ConsoleDocumentingVisitor: ASTVisitor {
 
     public init(){}
 
-    func visit(classDeclaration: ClassDeclaration) throws -> Bool {
-        print("Class Decl:  \(classDeclaration)")
+    public func visit(_ classDeclaration: ClassDeclaration) throws -> Bool {
+        print("Class Decl:  \(classDeclaration.name)")
+        return true
+    }
+
+    public func visit(_ initializer: InitializerDeclaration) throws -> Bool {
+        let debugString = 
+        """
+        Init:
+        modifier:\t\(initializer.modifiers)
+        args:\t\(initializer.parameterList)
+        """
+        print(debugString)
         return true
     }
 }
