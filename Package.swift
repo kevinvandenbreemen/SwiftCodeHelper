039a081f1db7342ef085bc4ba1bbc931dc3ddf01
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-11-21 18:36:00 -0500

    Include needed package

--- Package.swift
+++ Package.swift
@@ -7,13 +7,14 @@ let package = Package(
     name: "SwiftCodeHelper",
     dependencies: [
         .package(url: "https://github.com/yanagiba/swift-ast.git", from: "0.4.2"),
+        .package(url: "https://github.com/objecthub/swift-commandlinekit.git", from: "0.3.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftCodeHelper",
-            dependencies: ["SwiftAST+Tooling"]),
+            dependencies: ["SwiftAST+Tooling", "CommandLineKit"]),
         .target(name: "SwiftCodeHelperDemo",
             dependencies: ["SwiftCodeHelper"]
         ),
