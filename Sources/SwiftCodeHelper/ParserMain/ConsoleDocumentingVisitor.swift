6ecaccecf2b4373fb23b32de60c34d357669b762
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-11-21 09:12:34 -0500

    Explore more about the types of declarations a class has

--- Sources/SwiftCodeHelper/ParserMain/ConsoleDocumentingVisitor.swift
+++ Sources/SwiftCodeHelper/ParserMain/ConsoleDocumentingVisitor.swift
@@ -6,7 +6,22 @@ public class ConsoleDocumentingVisitor: ASTVisitor {
     public init(){}
 
     public func visit(_ classDeclaration: ClassDeclaration) throws -> Bool {
-        print("Class Decl:  \(classDeclaration.name)")
+
+        let members = classDeclaration.members
+        var memberString = "members:\n======================\n"
+
+        members.forEach{ member in 
+            switch member {
+            case let .declaration(declaration):
+                memberString += "\(type(of: declaration))\n"
+            case let .compilerControl(compilerControlStatement):
+                memberString += "Compiler Control: \(compilerControlStatement)\n"
+            default:
+                break
+            }
+        }
+
+        print("Class Decl:  \(classDeclaration.name), members:  \(memberString)")
         return true
     }
 
