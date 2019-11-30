eaf7d97943190830e9b53f58fa79caf155de9911
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-11-30 15:54:14 -0500

    Code cleanup

--- Sources/SwiftCodeHelper/View/Cocoa/DiagramView.swift
+++ Sources/SwiftCodeHelper/View/Cocoa/DiagramView.swift
@@ -94,9 +94,7 @@ class DiagramView: UIView {
         var clzIndex = 0
         modelArrangementRects.forEach{ rect in 
             logger.debug("Trying to create labelled box")
-            guard let lbr:model_arrangement_rect = rect.label_rect.pointee else {
-                return
-            }
+            let lbr:model_arrangement_rect = rect.label_rect.pointee
 
             let classRect = CGRect.init(x: CGFloat(rect.x), y: CGFloat(rect.y), width: CGFloat(rect.width), height: CGFloat(rect.height))
             let classLabelRect = CGRect.init(x: CGFloat(lbr.x), y: CGFloat(lbr.y), width: CGFloat(lbr.width), height: CGFloat(lbr.height))
@@ -112,30 +110,6 @@ class DiagramView: UIView {
             clzIndex += 1
 
         }
-
-        // let littleRectangle = CGRect.init(x: rect.origin.x + 10, y: rect.origin.y + 10, width: 100, height: 100)
-
-        
-
-        // let path = UIBezierPath(rect: rect)
-        // UIColor.green.setFill()
-        // path.fill()
-
-        // let littlePath = UIBezierPath.init(rect: littleRectangle)
-        // littlePath.lineWidth = CGFloat(6.0)
-        // littlePath.stroke()
-
-        // let textRect = CGRect.init(x: rect.origin.x + 200, y: rect.origin.y+10, width: 200, height: 40)
-        // let label = UILabel.init(frame: textRect)
-        // label.text = "Test Label"
-        // addSubview(label)
-
-        // UIColor.red.setStroke()
-        // let littleLineMaker = UIBezierPath()
-        // littleLineMaker.move(to: textRect.origin)
-        // littleLineMaker.addLine(to: littleRectangle.origin)
-        // littleLineMaker.lineWidth = 2
-        // littleLineMaker.stroke()
 
     }
     
