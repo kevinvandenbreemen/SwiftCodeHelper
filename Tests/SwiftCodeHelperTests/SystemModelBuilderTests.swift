b058864e4dfee1a1c9fa7e7335ca4e05eb6f15fd
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-12-15 10:36:02 -0500

    Additional validation

--- Tests/SwiftCodeHelperTests/SystemModelBuilderTests.swift
+++ Tests/SwiftCodeHelperTests/SystemModelBuilderTests.swift
@@ -67,11 +67,33 @@ class SystemModelBuilderTests: XCTestCase {
         XCTAssertEqual(interfaces[0].name, ifc.name)
     }
 
+    func testRegistersInterfaceImplementationBeforeClassOrInterfaceEncounteredClassThenInterfaceAdded() {
+        let builder = SystemModelBuilder()
+        
+        let ifc = Interface.init(name: "Interface")
+        let clz = Class.init(name: "Implementor")
+        
+        builder.addImplements(type: "Implementor", implements: "Interface")
+        builder.addClass(clz: clz)
+        builder.addInterface(interface: ifc)
+
+        let firstClass = builder.systemModel.classes[0]
+        
+        let interfaces = firstClass.interfaces
+        guard interfaces.count == 1 else {
+            XCTFail("Type \(firstClass.name) should have a single implemented interface")
+            return
+        }
+
+        XCTAssertEqual(interfaces[0].name, ifc.name)
+    }
+
 
     static var allTests = [
         ("Adds existing interface to a class", testDetectsInterfaceImplementation),
         ("Adds interface to a class even when interface not yet encountered at time of implements decl", testRegisteresInterfaceImplementationBeforeInterfaceEncountered),
-        ("Adds interface to a class even when neither the class nor the interface have been encountered at time of implements decl", testRegistersInterfaceImplementationBeforeClassOrInterfaceEncountered)
+        ("Adds interface to a class even when neither the class nor the interface have been encountered at time of implements decl", testRegistersInterfaceImplementationBeforeClassOrInterfaceEncountered),
+        ("Adds interface to a class when class then interface added after initial implementation", testRegistersInterfaceImplementationBeforeClassOrInterfaceEncounteredClassThenInterfaceAdded),
 
     ]
 
