b8884092d596d9fe4a8bc7b1625436fcabd2fb62
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-12-16 19:21:41 -0500

    Properly detect type inheritance

--- Sources/SwiftCodeHelper/ParserMain/SystemModellingVisitor.swift
+++ Sources/SwiftCodeHelper/ParserMain/SystemModellingVisitor.swift
@@ -1,17 +1,38 @@
 import AST
+import Logging
 
 public class SystemModellingVisitor: ASTVisitor {
+
+    private var logger: Logger
 
     private let builder: SystemModelBuilder
     
     public init(builder: SystemModelBuilder = SystemModelBuilder() ) {
         self.builder = builder
+        
+        self.logger = Logger.init(label: String(describing: SystemModellingVisitor.self))
+        self.logger.logLevel = .debug
     }
     
     public func visit(_ classDeclaration: ClassDeclaration)  throws -> Bool {
+
+        logger.debug("Class declaration:\n******************\n\(classDeclaration)")
+
         let className = String(describing: classDeclaration.name)
         let clz = Class(name: className)
         builder.addClass(clz: clz)
+
+        if let inheritanceClause = classDeclaration.typeInheritanceClause {
+            logger.debug("\(className):  Handling inheritance clause '\(inheritanceClause)'")
+            inheritanceClause.typeInheritanceList.forEach{ typeIdentifier in
+                let interfaceName = typeIdentifier.textDescription
+                logger.debug("adding '\(className) implements \(interfaceName)'")
+                builder.addImplements(type: className, implements: interfaceName)
+            }
+        }
+
+        logger.debug("******************")
+
         return true
     }
 
